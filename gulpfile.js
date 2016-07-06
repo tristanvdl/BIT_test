87dbab1d8720ff590f9360454a80363bd2bbc7e1
Tristan van der Linden <tristanvdl@gmail.com>
2016-07-07 00:28:41 +0200

    final

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+//sass
+gulp.task('sass', function() {
+    gulp.src('src/assets/style/app.scss')
+        .pipe(sass({
+            style: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('build/css'));
+});
+
+//javascript
+gulp.task('scripts', function() {
+    gulp.src('src/assets/js/app.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+//watch the files
+gulp.task('watch', function() {
+    gulp.watch('src/assets/style/scss/**/*.scss', ['sass']);
+    gulp.watch('src/assets/js/*.js', ['scripts']);
+});
+
+//default task
+gulp.task('default', ['sass', 'scripts', 'watch']);
